4672203b2041529e28aad0cb610a66be45cc8b12
willianproni <pronifwillian@gmail.com>
2025-07-19 15:21:23 -0300

    Feat - Create table meal and enum

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,13 @@
-import { date, integer, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+  date,
+  integer,
+  json,
+  pgEnum,
+  pgTable,
+  timestamp,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: uuid().primaryKey().defaultRandom(),
@@ -7,13 +16,37 @@ export const usersTable = pgTable("users", {
   password: varchar({ length: 255 }).notNull(),
   goal: varchar({ length: 8 }).notNull(),
   gender: varchar({ length: 6 }).notNull(),
-  birthDate: date('birth_date').notNull(),
+  birthDate: date("birth_date").notNull(),
   height: integer().notNull(),
   weight: integer().notNull(),
-  activityLevel: integer('activity_level').notNull(),
+  activityLevel: integer("activity_level").notNull(),
   // Goals
   calories: integer().notNull(),
   proteins: integer().notNull(),
   carbohydrates: integer().notNull(),
   fats: integer().notNull(),
+});
+
+export const mealStatus = pgEnum("meal_status", [
+  "uploading",
+  "queued",
+  "processing",
+  "success",
+  "failed",
+]);
+
+export const mealInputType = pgEnum("meal_input_type", ["audio", "picture"]);
+
+export const mealsTable = pgTable("meals", {
+  id: uuid().primaryKey().defaultRandom(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => usersTable.id, { onDelete: "cascade" }),
+  status: mealStatus().notNull(),
+  inputType: mealInputType("input_type").notNull(),
+  inputFileKey: varchar("input_file_key", { length: 255 }).notNull(),
+  name: varchar({ length: 255 }).notNull(),
+  icon: varchar({ length: 100 }).notNull(),
+  foods: json(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
 });
