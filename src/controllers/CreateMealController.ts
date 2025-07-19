49036542218a8aec5a914c50ff4649fdc04f559d
willianproni <pronifwillian@gmail.com>
2025-07-19 18:51:07 -0300

    Feat - Config Bucket S3 env and created url send attachment

--- src/controllers/CreateMealController.ts
+++ src/controllers/CreateMealController.ts
@@ -1,34 +1,58 @@
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { randomUUID } from "crypto";
+import { z } from "zod";
+import { s3Client } from "../clients/s3Client";
 import { db } from "../db";
 import { mealsTable } from "../db/schema";
 import { HttpResponse, ProtectedHttpRequest } from "../types/Http";
-import { badRequest, created, ok } from "../utils/http";
-import { z } from "zod";
+import { badRequest, created } from "../utils/http";
 
 const schema = z.object({
-  fileType: z.enum(['audio/m4a', 'image/jpeg']),
+  fileType: z.enum(["audio/m4a", "image/jpeg"]),
 });
 
 export class CreateMealController {
-  static async handle({ userId, body }: ProtectedHttpRequest): Promise<HttpResponse> {
+  static async handle({
+    userId,
+    body,
+  }: ProtectedHttpRequest): Promise<HttpResponse> {
     const { success, error, data } = schema.safeParse(body);
-    
+
     if (!success) {
       return badRequest({ errors: error.issues });
     }
+
+    const fileId = randomUUID();
+
+    const ext = data.fileType === "audio/m4a" ? ".m4a" : ".jpg";
+
+    const fileKey = `${fileId}${ext}`;
+
+    console.log("process:", process.env.BUCKET_NAME)
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.BUCKET_NAME,
+      Key: fileKey,
+    });
+
+    const presignedURL = await getSignedUrl(s3Client, command, {
+      expiresIn: 600,
+    });
 
     const [meal] = await db
       .insert(mealsTable)
       .values({
         userId,
-        inputFileKey: 'input_file_key',
-        inputType: data.fileType === 'audio/m4a' ? 'audio' : 'picture',
-        status: 'uploading',
-        icon: '',
-        name: '',
+        inputFileKey: fileKey,
+        inputType: data.fileType === "audio/m4a" ? "audio" : "picture",
+        status: "uploading",
+        icon: "",
+        name: "",
         foods: [],
       })
       .returning({ id: mealsTable.id });
 
-    return created({ mealId: meal.id });
+    return created({ mealId: meal.id, uploadUrl: presignedURL });
   }
 }
