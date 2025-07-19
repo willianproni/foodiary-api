3d537affb16deb9e8517d5b067344fc59326ad7e
willianproni <pronifwillian@gmail.com>
2025-07-19 12:34:18 -0300

    Feat - Valid email and password in signIn

--- src/controllers/SignInController.ts
+++ src/controllers/SignInController.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 
 import { HttpRequest, HttpResponse } from "../types/Http";
-import { badRequest, ok } from "../utils/http";
+import { badRequest, ok, unauthorized } from "../utils/http";
+import { db } from "../db";
+import { usersTable } from "../db/schema";
+import { eq } from "drizzle-orm";
+import { compare } from "bcryptjs";
 
 const schema = z.object({
   email: z.email(),
@@ -17,6 +21,25 @@ export class SingInController {
     const { success, error, data } = schema.safeParse(body);
 
     if (!success) return badRequest({ errors: error.issues });
+
+    const user = await db.query.usersTable.findFirst({
+      columns: {
+        id: true,
+        email: true,
+        password: true,
+      },
+      where: eq(usersTable.email, data.email),
+    });
+
+    if (!user) {
+      return unauthorized({ error: "Invalid credentials." });
+    }
+
+    const isPasswordValid = await compare(data.password, user.password);
+
+    if (!isPasswordValid) {
+      return unauthorized({ error: "Invalid credentials." });
+    }
 
     return ok({
       data,
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -27,3 +27,10 @@ export function conflict(body?: Record<string, any>): HttpResponse {
     body,
   };
 }
+
+export function unauthorized(body?: Record<string, any>): HttpResponse {
+  return {
+    statusCode: 401,
+    body,
+  };
+}
