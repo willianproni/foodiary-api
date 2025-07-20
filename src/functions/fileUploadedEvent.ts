820091135d9d9d1df641ce9fb19d125474a4d620
willianproni <pronifwillian@gmail.com>
2025-07-19 23:29:12 -0300

    Feat - Publish message in SQS Queue

--- src/functions/fileUploadedEvent.ts
+++ src/functions/fileUploadedEvent.ts
@@ -1,9 +1,16 @@
+import { SendMessageCommand } from "@aws-sdk/client-sqs";
 import { S3Event } from "aws-lambda";
+import { sqsClient } from "../clients/sqsClient";
 
 export async function handler(event: S3Event) {
   await Promise.all(
-    event.Records.map((record) => {
-      record.s3.object.key;
+    event.Records.map(async (record) => {
+      const command = new SendMessageCommand({
+        QueueUrl: process.env.MEALS_QUEUE_URL,
+        MessageBody: JSON.stringify({ fileKey: record.s3.object.key }),
+      });
+      
+      await sqsClient.send(command);
     })
   );
 }
